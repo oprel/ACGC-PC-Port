65f511975ef19d34e4461a949fdc11faf53a8a4c
oprel <jasper@oprel.work>
2026-07-29 20:54:51 +0200

    Can use D-Pad in more menus to make selection. Can use D-Pad to speed up/progress text.

--- src/game/m_submenu_ovl.c
+++ src/game/m_submenu_ovl.c
@@ -2141,6 +2141,11 @@ static void mSM_make_trigger_data(Submenu* submenu) {
     int trigger = (getButton() & 0xF) | getTrigger();
     mSM_Control_c* control = &submenu->overlay->menu_control;
 
+#ifdef PC_ENHANCEMENTS
+    /* D-Pad bits sit at the same positions as the C-stick bits, shifted left by 8 */
+    trigger |= (getButton() & (BUTTON_DUP | BUTTON_DDOWN | BUTTON_DLEFT | BUTTON_DRIGHT)) >> 8;
+#endif
+
     if (gamePT->mcon.move_pR > 0.5f) {
         u16 angle = gamePT->mcon.move_angle + DEG2SHORT_ANGLE2(45.0f);
 
